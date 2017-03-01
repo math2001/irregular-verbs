c208e9fbbd472e09259529ef3d57ec35f858836b
Mathieu PATUREL <australie.p@gmail.com>
2017-03-01 15:41:12 +1100

    improve code quality in question-manager

--- js/question-manager.js
+++ js/question-manager.js
@@ -4,10 +4,7 @@ class QuestionManager {
 
     static init() {
 
-        this.$infinite = $("#infinite")
-        this.$simple = $("#simple")
-        this.$participle = $("#participle")
-        this.$$inputs = [this.$infinite, this.$simple, this.$participle]
+        this.$$inputs = [$('#infinite'), $('#simple'), $('#participle')]
         this.$checkBtn = $('#check')
 
         this.bindDOM()
@@ -26,27 +23,28 @@ class QuestionManager {
                 // the answer were shown
                 this.$$inputs.forEach(function ($input) {
                     $input.parent().removeClass('valid').enabled(true)
-                })
+                }, this)
                 this.$checkBtn.text('Check!')
                 this.$checkBtn.data('action', '')
                 return this.ask()
             }
             this.check.call(this)
         })
-        this.$infinite.bind('keydown', submit)
-        this.$simple.bind('keydown', submit)
-        this.$participle.bind('keydown', submit)
+
+        this.$$inputs.forEach(function ($input) {
+            $input.bind('keydown', submit)
+        }, this)
     }
 
     static check() {
 
         const errorLevel = this.getErrorLevel()
-        const showAnswer = (errorLevel  == 1 && this.failedTimes == 3)
-                          || (errorLevel == 2 && this.failedTimes == 2)
 
         if (errorLevel > 0) {
             this.failedTimes ++
         }
+
+        const showAnswer = this.failedTimes > 2
 
         EM.emit('check question', {
             errorLevel: errorLevel,
@@ -60,63 +58,48 @@ class QuestionManager {
             this.ask()
         }
 
-        return
-
     }
 
     static showAnswer() {
-        this.$infinite.enabled(false).val(this.verb[0]).parent().addClass('valid')
-        this.$simple.enabled(false).val(this.verb[1]).parent().addClass('valid')
-        this.$participle.enabled(false).val(this.verb[2]).parent().addClass('valid')
+        this.$$inputs.forEach(function ($input, index) {
+            $input.enabled(false).val(this.verb[index]).parent().addClass('valid')
+        }, this)
         this.$checkBtn.data('action', 'ask').html('Ok... Next question please! &rarr;')
     }
 
     static getErrorLevel() {
-        let errorLevel = 0, value;
 
-        if (this.$infinite.attr('disabled') != 'disabled'
-            && this.$infinite.val().toLowerCase() != this.verb[0]) errorLevel++
+        let errorLevel = 0;
 
-        if (this.$simple.attr('disabled') != 'disabled'
-            && this.$simple.val().toLowerCase() != this.verb[1]) errorLevel++
-
-        if (this.$participle.attr('disabled') != 'disabled'
-            && !this.verb[2].includes(this.$participle.val().toLowerCase())) errorLevel++
+        this.$$inputs.forEach(function ($input, index) {
+            if ($input.attr('disabled') != 'disabled'
+                && $input.val().toLowerCase() != this.verb[index]) {
+                errorLevel ++
+            }
+        }, this)
 
         return errorLevel
+
     }
 
     static pickVerb() {
-        // this is where all the magic is going to happend
+        // this is where all the magic is going to happen
         const key = random.choice(Object.keys(irregurlarVerbs))
         return [key, irregurlarVerbs[key]['past'], irregurlarVerbs[key]['participles']]
     }
 
     static render() {
-        this.$infinite.val('').enabled(true)
-        this.$simple.val('').enabled(true)
-        this.$participle.val('').enabled(true)
+        this.$$input.forEach(function ($input) {
+            $input.val('').enabled(true)
+        }, this)
 
-        var $el
-        if (this.refIndex == 0) {
-            $el = this.$infinite
-        } else if (this.refIndex == 1) {
-            $el = this.$simple
-        } else if (this.refIndex == 2) {
-            $el = this.$participle
-        }
-
-        if (this.refIndex != 0) {
-            this.$infinite.focus()
-        } else {
-            this.$simple.focus()
-        }
+        this.$$inputs[this.refIndex !0 ? 0 : 1].focus()
 
         var val = this.verb[this.refIndex]
         if (typeof val != 'string') {
             val = val.join(' or ')
         }
-        $el.val(val).attr('disabled', 'true')
+        this.$$inputs[this.refIndex].val(val).enabled(false)
     }
 
     static ask() {
